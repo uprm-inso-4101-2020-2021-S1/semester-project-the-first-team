cd481fe494b13857b7ca197c507f1603f67658a5
Brian <brian.rodriguez17@upr.edu>
2020-09-24 19:06:46 -0400

    Initial commit to new stylist view branch.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,13 @@ import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   const temp = [
     { name: "home", link: "/home" },
-    { name: "reservations", link: "/reservations" }
+    { name: "reservations", link: "/reservations" },
   ];
   return (
     <Router>
       <Sidebar items={temp} />
     </Router>
+    // Test to create stylist-view branch
   );
 }
 
