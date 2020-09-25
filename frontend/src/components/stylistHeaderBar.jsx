e3108de26ebe14dda657c6341cdc4e1836110beb
Brian <brian.rodriguez17@upr.edu>
2020-09-25 13:20:12 -0400

    improvements to headerbar scss

--- frontend/src/components/stylistHeaderBar.jsx
+++ frontend/src/components/stylistHeaderBar.jsx
@@ -14,27 +14,25 @@ class StylistHeaderBar extends Component {
 
   render() {
     return (
-      <nav className="navbar navbar-inverse navbar-expand-lg navbar-dark stylist-headerbar">
-        <div className="headerbar-content">
-          <div>
-            <a className="navbar-brand" href="#">
-              {this.state.title[this.props.currentView]}
-            </a>
-          </div>
-          {/* TODO: Figure out how to get card to not overflow into clock when resizing page. */}
-          <div className="headerbar-card-div">
-            <span>
-              <HeaderBarCard />
-            </span>
-          </div>
+      <nav className="navbar navbar-inverse navbar-expand-lg navbar-dark stylist-headerbar headerbar-content">
+        <div>
+          <a className="navbar-brand" href="#">
+            {this.state.title[this.props.currentView]}
+          </a>
+        </div>
+        {/* TODO: Figure out how to get card to not overflow into clock when resizing page. */}
+        <div className="headerbar-card-div">
+          <span>
+            <HeaderBarCard />
+          </span>
+        </div>
 
-          {/* This code renders the divider for the clock */}
-          {/* TODO: Figure out how to be fixed to right of component, not page. */}
-          <div className="headerbar-clock-div">
-            <span>
-              <HeaderBarClock />
-            </span>
-          </div>
+        {/* This code renders the divider for the clock */}
+        {/* TODO: Figure out how to be fixed to right of component, not page. */}
+        <div className="headerbar-clock-div">
+          <span>
+            <HeaderBarClock />
+          </span>
         </div>
       </nav>
     );
