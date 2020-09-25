94df4de7dfa63f18a1be540f4fed3ebbc97f266f
Brian <brian.rodriguez17@upr.edu>
2020-09-25 13:49:45 -0400

    Fixed part of why sidebar hid elements.

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -4,13 +4,13 @@ import { Container, Nav } from "react-bootstrap";
 class Sidebar extends React.Component {
   render() {
     return (
-      <Container fluid className="flex-column">
+      <Container fluid className="flex-column sidebar-column">
         <Nav id="sidebar">
           <div className="sidebar-header">
             <h3>Express Cuts</h3>
           </div>
           <div className="sidebar-items justify-content-center">
-            {this.props.items.map(item => (
+            {this.props.items.map((item) => (
               <Nav.Item>
                 <Nav.Link href={item.link}>{item.name}</Nav.Link>
               </Nav.Item>
