5d66fea68efdad1f020deaa0e75e518185f63fba
Brian <brian.rodriguez17@upr.edu>
2020-09-27 17:49:03 -0400

    created base card for body form

--- frontend/src/components/stylistViewForm.jsx
+++ frontend/src/components/stylistViewForm.jsx
@@ -6,9 +6,33 @@ class StylistViewForm extends Component {
     return (
       <card className="stylist-body-card">
         <body className="card-body stylist-body-card-body">
-          <a className="card-header">Testing part 2</a>
+          <a className="card-header">New Stylist Form</a>
+          <form>
+            <label>
+              First Name:
+              <input type="text" name="fname" />
+            </label>
+            <label>
+              Last Name:
+              <input type="text" name="lname" />
+            </label>
 
-          <a className="card-text">Testing part 2</a>
+            <label>
+              E-mail:
+              <input type="text" name="email" />
+            </label>
+
+            <label>
+              Password:
+              <input type="password" name="pswd" />
+            </label>
+
+            <label>
+              Confirm Password:
+              <input type="password" name="confirm-pswd" />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
         </body>
       </card>
     );
