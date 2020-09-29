028a8eeb465f009ab29e6000e56da06b67876c3f
Brian <brian.rodriguez17@upr.edu>
2020-09-29 18:17:49 -0400

    improved layout of queue cards

--- frontend/src/components/stylistAppointmentQueue.jsx
+++ frontend/src/components/stylistAppointmentQueue.jsx
@@ -9,6 +9,29 @@ class stylistAppointmentQueue extends Component {
           "https://images.pexels.com/photos/1841819/pexels-photo-1841819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
         username: "Tris Everdeen",
         appTime: "3:30 P.M.",
+        estWait: 15,
+        services: ["Shampoo", "conditioner", "blower", "manicure", "trim"],
+        comments: "",
+      },
+      {
+        //   Temp royalty free profile picture
+        profilePic:
+          "https://images.pexels.com/photos/1841819/pexels-photo-1841819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        username: "Tris Everdeen",
+        appTime: "3:30 P.M.",
+        estWait: 15,
+        services: ["Shampoo", "conditioner", "blower", "manicure", "trim"],
+        comments: "",
+      },
+      {
+        //   Temp royalty free profile picture
+        profilePic:
+          "https://images.pexels.com/photos/1841819/pexels-photo-1841819.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+        username: "Alexandria Doubledaddario",
+        appTime: "3:30 P.M.",
+        estWait: 15,
+        services: ["Shampoo", "conditioner", "blower", "manicure", "trim"],
+        comments: "",
       },
     ],
   };
@@ -21,21 +44,69 @@ class stylistAppointmentQueue extends Component {
             style={{
               display: "flex",
               flexWrap: "nowrap",
+              margin: "1rem",
             }}
           >
-            <div>
-              <card className="card">
+            <div style={{ paddingRight: "1rem" }}>
+              <card
+                className="card"
+                style={{
+                  height: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  padding: "1rem",
+                }}
+              >
                 <a>{appointment.appTime}</a>
               </card>
             </div>
             <card className=" header-card">
-              <body className="card-body header-card-body">
-                <picture style={{ padding: 5 }}>
-                  <img src={appointment.profilePic}></img>
+              <body
+                className="card-body header-card-body"
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "nowrap",
+                  justifyContent: "center",
+                }}
+              >
+                <picture
+                  style={{
+                    padding: 5,
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                  }}
+                >
+                  <img src={appointment.profilePic} style={{}}></img>
                 </picture>
-                <a className="card-text">{appointment.username}</a>
-                <a className="card-div" />
-                <a className="card-text">Testing part 2</a>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                  }}
+                >
+                  <a className="card-text">{appointment.username}</a>
+                </div>
+                <a className="card-div" style={{ height: "100%" }} />
+                {/* TODO: Figure out how to wrap long names. */}
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    flexWrap: "wrap",
+                  }}
+                >
+                  <a className="card-text">
+                    Num. of Services: {appointment.services.length}
+                  </a>
+                  <a className="card-text">
+                    Est. Duration: {appointment.estWait} min.
+                  </a>
+                  <a className="card-text">Status: Waiting</a>
+                </div>
               </body>
             </card>
           </div>
