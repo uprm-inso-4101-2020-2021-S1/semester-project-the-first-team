3b9f094d4e1b9c45f0187fd53852df20e2e281c8
Milton Pagan <milton.pagan1@upr.edu>
2020-09-30 15:32:20 -0400

    Add sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import Sidebar from "./components/Sidebar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { faHome, faConciergeBell } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
+  //Delete this later on
   const temp = [
-    { name: "home", link: "/home" },
-    { name: "reservations", link: "/reservations" }
+    { title: "home", path: "/home", icon: faHome, cName: "nav-text" },
+    {
+      title: "reservations",
+      path: "/reservations",
+      icon: faConciergeBell,
+      cName: "nav-text"
+    }
   ];
   return (
     <Router>
       <Sidebar items={temp} />
+      <Switch>
+        <Route path="/" />
+      </Switch>
     </Router>
   );
 }
--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,25 +1,53 @@
-import React from "react";
-import { Container, Nav } from "react-bootstrap";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { Container, Nav, Row, Col } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCut, faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
-class Sidebar extends React.Component {
-  render() {
-    return (
-      <Container fluid className="flex-column">
-        <Nav id="sidebar">
-          <div className="sidebar-header">
-            <h3>Express Cuts</h3>
+function Sidebar(props) {
+  const [sidebar, setSidebar] = useState(false);
+
+  const showSidebar = () => setSidebar(!sidebar);
+
+  return (
+    <>
+      <div className="navbar">
+        <Link to="#" className="menu-bars">
+          <FontAwesomeIcon onClick={showSidebar} icon={faBars} />
+        </Link>
+      </div>
+      <Nav className={sidebar ? "nav-menu active" : "nav-menu"}>
+        <div className="nav-menu-items">
+          <div className="navbar-toggle">
+            <Link to="#" className="menu-close">
+              <FontAwesomeIcon
+                onClick={showSidebar}
+                icon={faTimes}
+              />
+            </Link>
           </div>
-          <div className="sidebar-items justify-content-center">
-            {this.props.items.map(item => (
-              <Nav.Item>
-                <Nav.Link href={item.link}>{item.name}</Nav.Link>
+          <div className="nav-menu-header">
+            <FontAwesomeIcon icon={faCut} className="icon" />
+            <h3>express cuts</h3>
+          </div>
+          {props.items.map((item, index) => {
+            return (
+              <Nav.Item
+                onClick={showSidebar}
+                key={index}
+                className={item.cName}
+              >
+                <Nav.Link href={item.path}>
+                  <FontAwesomeIcon icon={item.icon} />
+                  <span>{item.title}</span>
+                </Nav.Link>
               </Nav.Item>
-            ))}
-          </div>
-        </Nav>
-      </Container>
-    );
-  }
+            );
+          })}
+        </div>
+      </Nav>
+    </>
+  );
 }
 
 export default Sidebar;
