05c8ef47e04a60d140f1f7ef69442b9c98fde1b0
Brian <brian.rodriguez17@upr.edu>
2020-10-07 12:11:48 -0400

    Styling improvements to Active app. view

--- frontend/src/components/activeAppointmentView.jsx
+++ frontend/src/components/activeAppointmentView.jsx
@@ -1,5 +1,21 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import {
+  faSoap,
+  faPumpSoap,
+  faWind,
+  faHandSparkles,
+  faHandScissors,
+} from "@fortawesome/free-solid-svg-icons";
+
+const SERVICEICONS = {
+  Shampoo: faSoap,
+  Conditioner: faPumpSoap,
+  Blower: faWind,
+  Manicure: faHandSparkles,
+  Trim: faHandScissors,
+};
 class ActiveAppointmentView extends Component {
   state = {
     //   Temp royalty free profile picture
@@ -18,6 +34,7 @@ class ActiveAppointmentView extends Component {
     comments: "Used to have a little shampoo, now I have a lot!",
     stylist: "Jenna Fromdablok",
   };
+
   render() {
     return (
       <div
@@ -27,38 +44,60 @@ class ActiveAppointmentView extends Component {
           width: "100%",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            width: "75%",
-            height: "75vh",
-            alignContent: "space-between",
-            justifyContent: "center",
-            flexWrap: "wrap",
-            overflowX: "hidden",
-            overflowY: "auto",
-          }}
-        >
-          {this.state.services.map((service) => (
-            <card
-              className="service-card"
-              style={{
-                width: "20rem",
-                height: "20rem",
-                backgroundColor: "white",
-                margin: "10px",
-              }}
-            >
-              <a>{service.servName}</a> <button>Start</button>
-              <button>Delete</button>
-            </card>
-          ))}
+        <div style={{ width: "75%" }}>
+          <a style={{ color: "white" }}>Services:</a>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+
+              height: "75vh",
+              alignContent: "space-between",
+              justifyContent: "center",
+              flexWrap: "wrap",
+              overflowX: "hidden",
+              overflowY: "auto",
+            }}
+          >
+            {this.state.services.map((service) => (
+              <card
+                className="service-card"
+                style={{
+                  width: "16rem",
+                  height: "16rem",
+                  backgroundColor: "white",
+                  margin: "5px",
+                  display: "flex",
+                  flexDirection: "column",
+                  flexWrap: "nowrap",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  borderRadius: "12px",
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={SERVICEICONS[service.servName]}
+                  style={{ height: "40%", width: "40%" }}
+                />
+                <a>{service.servName}</a>
+                <div>
+                  <button>Start</button>
+                  <button>Delete</button>
+                </div>
+              </card>
+            ))}
+          </div>
         </div>
         <div>
-          <a style={{ color: "white" }}>Comments:</a>
-          <div className="card">
-            <a>{this.state.comments}</a>
+          <div style={{ height: "70%" }}>
+            <a style={{ color: "white" }}>Comments:</a>
+            <div className="card">
+              <a>{this.state.comments}</a>
+            </div>
+          </div>
+          <div>
+            <button>Finish</button>
+            <button>Cancel</button>
           </div>
         </div>
       </div>
