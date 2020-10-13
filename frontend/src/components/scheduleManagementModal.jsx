4c5944704aad8e5da8d55a3d6c46a053ef6f35bd
Brian <brian.rodriguez17@upr.edu>
2020-10-12 21:51:11 -0400

    Extracted Schedule Managment Modal and removed redundancies.

---
+++ frontend/src/components/scheduleManagementModal.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+function ScheduleManagementModal(props) {
+  return (
+    <Modal show={props.showModal} onHide={props.onHide}>
+      <Modal.Header closeButton>
+        <Modal.Title>
+          {props.showEditModal
+            ? props.event.title
+            : "Create Stylist Schedule Block:"}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div>
+          <div>
+            <h5>Schedule: </h5>
+          </div>
+          <div>
+            <a style={{ fontWeight: "bold" }}>Start: </a>
+            <span>
+              {props.event.start ? props.event.start.toDateString() : ""}{" "}
+            </span>
+            <span>
+              {props.event.start ? props.event.start.toTimeString() : ""}
+            </span>
+          </div>
+          <div>
+            <a style={{ fontWeight: "bold" }}>End: </a>
+            <span>
+              {props.event.end ? props.event.end.toDateString() : ""}{" "}
+            </span>
+            <span>{props.event.end ? props.event.end.toTimeString() : ""}</span>
+          </div>
+        </div>
+        {!props.showEditModal && (
+          <Form onSubmit={props.createEvent}>
+            <Form.Label>Select Stylist:</Form.Label>
+            <Form.Control as="select" name="stylistName" type="select">
+              {props.stylistNames.map((stylistName) => (
+                <option>{stylistName}</option>
+              ))}
+            </Form.Control>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        )}
+      </Modal.Body>
+      {props.showEditModal && (
+        <Modal.Footer>
+          <Button type="delete" name="delete" onClick={props.deleteEvent}>
+            Delete
+          </Button>
+        </Modal.Footer>
+      )}
+    </Modal>
+  );
+}
+export default ScheduleManagementModal;
