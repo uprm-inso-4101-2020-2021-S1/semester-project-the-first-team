c6fe2d3108f57a1fca9d8b085930df22cb9f06f9
Brian <brian.rodriguez17@upr.edu>
2020-10-14 18:53:05 -0400

    extracted stats view constants from responsive bar. Cannot get legend to display.

--- frontend/src/components/statsView.jsx
+++ frontend/src/components/statsView.jsx
@@ -1,5 +1,80 @@
-import React, { Component, Fragment } from "react";
-import ResponsiveBar from "./../../node_modules/nivo/lib/components/charts/bar/ResponsiveBar";
+import React, { Component } from "react";
+import ResponsiveBar from "nivo/lib/components/charts/bar/ResponsiveBar";
+
+const MARGIN = { top: 50, right: 60, bottom: 50, left: 60 };
+const PATTERNDEFS = [
+  {
+    id: "dots",
+    type: "patternDots",
+    background: "inherit",
+    color: "#38bcb2",
+    size: 4,
+    padding: 1,
+    stagger: true,
+  },
+  {
+    id: "lines",
+    type: "patternLines",
+    background: "inherit",
+    color: "#eed312",
+    rotation: -45,
+    lineWidth: 6,
+    spacing: 10,
+  },
+];
+const PATTERNASSIGNMENTS = [
+  {
+    match: {
+      id: "trim",
+    },
+    id: "dots",
+  },
+  {
+    match: {
+      id: "Jodie",
+    },
+    id: "lines",
+  },
+];
+const AXISBOTTOM = {
+  tickSize: 10,
+  tickPadding: 5,
+  legend: "Stylist",
+  legendPosition: "middle",
+  legendOffset: 32,
+};
+const AXISLEFT = {
+  tickSize: 5,
+  tickPadding: 5,
+  tickRotation: 0,
+  legend: "Avg. Duration (minutes)",
+  legendOffset: -40,
+};
+
+// TODO; FIGURE OUT HOW TO GET LEGEND TO SHOW UP
+// const LEGEND = {
+//   dataFrom: "keys",
+//   anchor: "bottom-right",
+//   direction: "column",
+//   justify: false,
+//   // translateX: 120,
+//   // translateY: 0,
+//   itemsSpacing: 2,
+//   itemWidth: 100,
+//   itemHeight: 20,
+//   itemDirection: "left-to-right",
+//   // itemOpacity: 0.85,
+//   symbolSize: 20,
+//   effects: [
+//     {
+//       on: "hover",
+//       style: {
+//         itemOpacity: 1,
+//       },
+//     },
+//   ],
+// };
+
 class StatsView extends Component {
   state = {
     data: [
@@ -22,99 +97,31 @@ class StatsView extends Component {
     ],
     keys: ["iron", "manicure", "shampoo", "conditioner", "trim"],
     indexBy: "stylist",
-    axisBottom: "Stylist",
-    axisLeft: "Avg. Duration (minutes)",
   };
   render() {
     return (
-      <div style={{ width: "100%", height: "60vh" }}>
+      <div
+        style={{
+          width: "50vw",
+          height: "60vh",
+          backgroundColor: "white",
+          borderRadius: "6px",
+        }}
+      >
         <ResponsiveBar
           data={this.state.data}
           keys={this.state.keys}
           indexBy={this.state.indexBy}
-          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          // STYLE =====================
+          margin={MARGIN}
           padding={0.3}
-          //   colors={{ scheme: "nivo" }}
-          defs={[
-            {
-              id: "dots",
-              type: "patternDots",
-              background: "inherit",
-              color: "#38bcb2",
-              size: 4,
-              padding: 1,
-              stagger: true,
-            },
-            {
-              id: "lines",
-              type: "patternLines",
-              background: "inherit",
-              color: "#eed312",
-              rotation: -45,
-              lineWidth: 6,
-              spacing: 10,
-            },
-          ]}
-          fill={[
-            {
-              match: {
-                id: "Anne",
-              },
-              id: "dots",
-            },
-            {
-              match: {
-                id: "Jodie",
-              },
-              id: "lines",
-            },
-          ]}
-          //   borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-          axisTop={null}
-          axisRight={null}
-          axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: this.state.axisBottom,
-            legendPosition: "middle",
-            legendOffset: 32,
-          }}
-          axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legend: this.state.axisLeft,
-            legendPosition: "middle",
-            legendOffset: -40,
-          }}
+          defs={PATTERNDEFS}
+          fill={PATTERNASSIGNMENTS}
+          axisBottom={AXISBOTTOM}
+          axisLeft={AXISLEFT}
           labelSkipWidth={12}
           labelSkipHeight={12}
-          //   labelTextColor={{ from: "color", modifiers: [["darker", 1.6]] }}
-          legends={[
-            {
-              dataFrom: "keys",
-              anchor: "bottom-right",
-              direction: "column",
-              justify: false,
-              translateX: 20,
-              translateY: 0,
-              itemsSpacing: 2,
-              itemWidth: 100,
-              itemHeight: 20,
-              itemDirection: "left-to-right",
-              itemOpacity: 0.85,
-              symbolSize: 20,
-              effects: [
-                {
-                  on: "hover",
-                  style: {
-                    itemOpacity: 1,
-                  },
-                },
-              ],
-            },
-          ]}
+          // legends={[LEGEND]}
           animate={true}
           motionStiffness={90}
           motionDamping={15}
