256d487e2bc49c7eb4f0fb4705a579a209dd41e9
Milton Pagan <milton.pagan1@upr.edu>
2020-10-28 21:05:47 -0400

    Added fetch hooks base code for backend connection

--- frontend/src/components/CustomerReservation.js
+++ frontend/src/components/CustomerReservation.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Alert, Container, Row, Col } from "react-bootstrap";
 import PropTypes from "prop-types";
 import ReservationForm from "./ReservationForm";
@@ -7,11 +7,87 @@ import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { SlideDown } from "react-slidedown";
 import "react-slidedown/lib/slidedown.css";
+import axios from "axios";
 
 const numStages = 4;
+
+const temp = [
+  {
+    name: "Service0",
+    icon: faArrowRight,
+  },
+  {
+    name: "Service1",
+    icon: faArrowRight,
+  },
+  {
+    name: "Service2",
+    icon: faArrowRight,
+  },
+  {
+    name: "Service3",
+    icon: faArrowRight,
+  },
+  {
+    name: "Service4",
+    icon: faArrowRight,
+  },
+  {
+    name: "Service5",
+    icon: faArrowRight,
+  },
+];
+
+const temp2 = [
+  {
+    name: "Maria",
+    description: "lorem ipsilum",
+    portrait:
+      "https://i.pinimg.com/originals/80/e3/86/80e3869ea24b00b264ff0b075d1e9384.jpg",
+  },
+  {
+    name: "Juana Diaz",
+    description: "lorem ipsilum",
+    portrait:
+      "https://i.pinimg.com/564x/3e/2e/8c/3e2e8c6fa626636eb4e8bdfe78edab3b.jpg",
+  },
+  {
+    name: "Maria",
+    description: "lorem ipsilum",
+    portrait:
+      "https://i.pinimg.com/originals/80/e3/86/80e3869ea24b00b264ff0b075d1e9384.jpg",
+  },
+];
+
+const dummyDate = new Date("2020-10-19 13:30");
+const temp3 = [
+  {
+    stylistName: "Maria",
+    time: dummyDate,
+    timeSlotId: 1,
+  },
+  {
+    stylistName: "Maria",
+    time: dummyDate,
+    timeSlotId: 1,
+  },
+  {
+    stylistName: "Maria",
+    time: dummyDate,
+    timeSlotId: 1,
+  },
+];
+
 function CustomerReservation(props) {
+  const [services, setServices] = useState(null);
+  const [stylists, setStylists] = useState(null);
+  const [timeSlots, setTimeSlots] = useState(null);
+
   const [reservationStage, setReservationStage] = useState(0);
   const [stageMinimumSelected, setStageMinimumSelected] = useState(
+    Array(numStages).fill(false)
+  );
+  const [stageSelectionChanged, setStageSelectionChanged] = useState(
     Array(numStages).fill(false)
   );
   const [submitted, setSubmitted] = useState(false);
@@ -24,6 +100,68 @@ function CustomerReservation(props) {
     "Select a time slot",
     "Review your reservation",
   ];
+
+  // Functions
+  const stageChanged = useCallback(
+    (stage, value = true) => {
+      stageSelectionChanged[stage] = value;
+      setStageSelectionChanged(stageSelectionChanged);
+    },
+    [stageSelectionChanged]
+  );
+
+  // Get services
+
+  useEffect(() => {
+    if (services === null && reservationStage === 0) {
+      const getServices = async () => {
+        setLoading(true);
+        const result = await axios(
+          "https://hn.algolia.com/api/v1/search?query=redux"
+        );
+        console.log("services");
+        setServices(temp);
+        setLoading(false);
+      };
+      getServices();
+    }
+  }, [reservationStage, services]);
+
+  // Get stylists
+
+  useEffect(() => {
+    if (reservationStage === 1 && stageSelectionChanged[0]) {
+      const getStylists = async () => {
+        setLoading(true);
+        const result = await axios(
+          "https://hn.algolia.com/api/v1/search?query=redux"
+        );
+        console.log("stylists");
+        setStylists(temp2);
+        setLoading(false);
+      };
+      getStylists();
+      stageChanged(0, false);
+    }
+  }, [reservationStage, stageChanged, stageSelectionChanged]);
+
+  // Get time slots
+
+  useEffect(() => {
+    if (reservationStage === 2 && stageSelectionChanged[1]) {
+      const getTimeSlots = async () => {
+        setLoading(true);
+        const result = await axios(
+          "https://hn.algolia.com/api/v1/search?query=redux"
+        );
+        console.log("time");
+        setTimeSlots(temp3);
+        setLoading(false);
+      };
+      getTimeSlots();
+      stageChanged(1, false);
+    }
+  }, [reservationStage, stageChanged, stageSelectionChanged]);
 
   return (
     <Container fluid>
@@ -92,26 +230,23 @@ function CustomerReservation(props) {
             ) : null}
           </SlideDown>
           <ReservationForm
-            services={props.services}
-            stylists={props.stylists}
+            loading={loading}
+            services={services}
+            stylists={stylists}
             submitted={submitted}
-            timeSlots={props.timeSlots}
+            timeSlots={timeSlots}
             reservationStage={reservationStage}
             setReservationStage={setReservationStage}
             setSubmitted={setSubmitted}
             stageMinimumSelected={stageMinimumSelected}
             setStageMinimumSelected={setStageMinimumSelected}
+            stageChanged={stageChanged}
+            stageSelectionChanged={stageSelectionChanged}
           />
         </Container>
       </Row>
     </Container>
   );
 }
-
-CustomerReservation.propTypes = {
-  services: PropTypes.array,
-  stylists: PropTypes.array,
-  timeSlots: PropTypes.array,
-};
 
 export default CustomerReservation;
--- frontend/src/components/ReservationForm.js
+++ frontend/src/components/ReservationForm.js
@@ -1,39 +1,51 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import CustomerReservationServices from "./CustomerReservationServices";
 import CustomerRersevationStylists from "./CustomerReservationStylists";
 import PropTypes from "prop-types";
 import CustomerResevationTimeSlots from "./CustomerReservationTimeSlots";
 import CustomerReservationSummary from "./CustomerReservationSummary";
 import { Link } from "react-router-dom";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Spinner } from "react-bootstrap";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const maxServices = 3;
 
 function ReservationForm(props) {
-
   // State variables
   const [serviceNames, setServiceNames] = useState([]);
-  const [serviceIsActive, setServiceIsActive] = useState(
-    props.services.map(() => {
-      return false;
-    })
-  );
+  const [serviceIsActive, setServiceIsActive] = useState([]);
   const [activeServiceCount, setActiveServiceCount] = useState(0);
   const [stylistName, setStylistName] = useState("");
-  const [portraitIsActive, setPortraitIsActive] = useState(
-    props.stylists.map(() => {
-      return false;
-    })
-  );
-
+  const [portraitIsActive, setPortraitIsActive] = useState([]);
   const [timeSlotId, setTimeSlotId] = useState(null);
-  const [timeSlotIsActive, setTimeSlotIsActive] = useState(
-    props.timeSlots.map(() => {
-      return false;
-    })
-  );
+  const [timeSlotIsActive, setTimeSlotIsActive] = useState([]);
+
+  // Initialize arrays once props are available or reset once they change
+  useEffect(() => {
+    if (serviceIsActive.length === 0 && props.services) {
+      setServiceIsActive(Array(props.services.length).fill(false));
+    }
+  }, [serviceIsActive, props.services]);
+
+  console.log(props.stageSelectionChanged);
+  useEffect(() => {
+    if (
+      (portraitIsActive.length === 0 || props.stageSelectionChanged[0]) &&
+      props.stylists
+    ) {
+      setPortraitIsActive(Array(props.stylists.length).fill(false));
+    }
+  }, [portraitIsActive, props.stylists, props.stageSelectionChanged]);
+
+  useEffect(() => {
+    if (
+      (timeSlotIsActive.length === 0 || props.stageSelectionChanged[1]) &&
+      props.timeSlots
+    ) {
+      setTimeSlotIsActive(Array(props.timeSlots.length).fill(false));
+    }
+  }, [timeSlotIsActive, props.timeSlots, props.stageSelectionChanged]);
 
   // Handlers
   const updateStageMinimumSelected = (status) => {
@@ -63,6 +75,7 @@ function ReservationForm(props) {
       setActiveServiceCount(activeServiceCount - 1);
     }
     updateStageMinimumSelected(serviceIsActive.includes(true));
+    props.stageChanged(props.reservationStage);
   };
 
   const setPortraitActive = (index) => {
@@ -74,6 +87,7 @@ function ReservationForm(props) {
     );
     if (!props.stageMinimumSelected[props.reservationStage])
       updateStageMinimumSelected(true);
+    props.stageChanged(props.reservationStage);
   };
 
   const setTimeSlotActive = (index) => {
@@ -113,42 +127,65 @@ function ReservationForm(props) {
     setTimeSlotId(id);
   };
 
+  const dataAvailable = () => {
+    switch (props.reservationStage) {
+      case 0:
+        return props.services;
+      case 1:
+        return props.stylists;
+      case 2:
+        return props.timeSlots;
+
+      default:
+        return true;
+    }
+  };
+
   const handleSubmit = () => {
     props.setSubmitted(true);
   };
 
   return (
     <form onSubmit={handleSubmit} method="POST">
-      {!props.submitted && props.reservationStage === 0 && (
-        <CustomerReservationServices
-          name="serviceName"
-          getServiceName={getSelectedService}
-          services={props.services}
-          cardActive={serviceIsActive}
-          setActive={setServiceActive}
-        />
-      )}
-      {!props.submitted && props.reservationStage === 1 && (
-        <CustomerRersevationStylists
-          name="stylistName"
-          getStylistName={getSelectedStylist}
-          stylists={props.stylists}
-          portraitIsActive={portraitIsActive}
-          setActive={setPortraitActive}
-        />
-      )}
-      {!props.submitted && props.reservationStage === 2 && (
-        <CustomerResevationTimeSlots
-          name="timeSlotId"
-          getTimeSlotId={getSelectedTimeSlot}
-          timeSlots={props.timeSlots}
-          timeSlotIsActive={timeSlotIsActive}
-          setActive={setTimeSlotActive}
-          reservationStage={props.reservationStage}
-          setReservationStage={props.setReservationStage}
-        />
-      )}
-      {!props.submitted && props.reservationStage === 3 && (
+      {dataAvailable() &&
+        !props.loading &&
+        !props.submitted &&
+        props.reservationStage === 0 && (
+          <CustomerReservationServices
+            name="serviceName"
+            getServiceName={getSelectedService}
+            services={props.services}
+            cardActive={serviceIsActive}
+            setActive={setServiceActive}
+          />
+        )}
+      {dataAvailable() &&
+        !props.loading &&
+        !props.submitted &&
+        props.reservationStage === 1 && (
+          <CustomerRersevationStylists
+            name="stylistName"
+            getStylistName={getSelectedStylist}
+            stylists={props.stylists}
+            portraitIsActive={portraitIsActive}
+            setActive={setPortraitActive}
+          />
+        )}
+      {dataAvailable() &&
+        !props.loading &&
+        !props.submitted &&
+        props.reservationStage === 2 && (
+          <CustomerResevationTimeSlots
+            name="timeSlotId"
+            getTimeSlotId={getSelectedTimeSlot}
+            timeSlots={props.timeSlots}
+            timeSlotIsActive={timeSlotIsActive}
+            setActive={setTimeSlotActive}
+            reservationStage={props.reservationStage}
+            setReservationStage={props.setReservationStage}
+          />
+        )}
+      {!props.loading && !props.submitted && props.reservationStage === 3 && (
         <CustomerReservationSummary
           selectedServices={serviceNames}
           selectedStylist={stylistName}
@@ -158,7 +195,7 @@ function ReservationForm(props) {
           handleSubmit={handleSubmit}
         />
       )}
-      {props.submitted && (
+      {!props.loading && props.submitted && (
         <Row>
           <Col className="reservation-end justify-content-center text-center">
             <Row>
@@ -182,17 +219,31 @@ function ReservationForm(props) {
           </Col>
         </Row>
       )}
+      {(props.loading || !dataAvailable()) && (
+        <Row>
+          <Col className="justify-content-center align-items-center text-center loading">
+            <Spinner
+              animation="border"
+              variant="secondary"
+              style={{ width: "60px", height: "60px" }}
+            />
+          </Col>
+        </Row>
+      )}
     </form>
   );
 }
 
 ReservationForm.propTypes = {
+  loading: PropTypes.bool,
   reservationStage: PropTypes.number,
   services: PropTypes.array,
   setReservationStage: PropTypes.func,
   setStageMinimumSelected: PropTypes.func,
+  stageChanged: PropTypes.func,
   setSubmitted: PropTypes.func,
   stageMinimumSelected: PropTypes.array,
+  stageSelectionChanged: PropTypes.array,
   stylists: PropTypes.array,
   submitted: PropTypes.bool,
   timeSlots: PropTypes.array,
