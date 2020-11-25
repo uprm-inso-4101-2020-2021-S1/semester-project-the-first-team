9adaf904270e77450e3b796e0e7931df7636f85e
yarm369 <yacenia.rios@upr.edu>
2020-11-25 04:40:37 -0400

    Admin Site models declarations.

--- express_cut/express_service/admin.py
+++ express_cut/express_service/admin.py
@@ -1,27 +1,40 @@
 from django.contrib import admin
-from .models import User, Service, StylistOfferServices, DailySchedule, Reservation, Notification
+from .models import User, Service, StylistOfferServices, DailySchedule, Reservation, Notification, \
+    ReservationContainsServices, TimeSlot
 from django.contrib import admin
-# Register your models here.
 
 
-# @admin.register(Stylist)
-# class StylistAdmin(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name', 'profile_details',)
-#
-#
-# @admin.register(Customer)
-# class Client(admin.ModelAdmin):
-#     list_display = ('first_name', 'last_name',)
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'role')
+    list_filter = ('role', )
+    exclude = ('groups', 'user_permissions')
+
+
+class TimeSlotInline(admin.TabularInline):
+    model = TimeSlot
+    extra = 1
+
+
+@admin.register(DailySchedule)
+class DailyScheduleAdmin(admin.ModelAdmin):
+    list_display = ('id', 'date', 'stylist',)
+    list_filter = ('date', )
+    inlines = (TimeSlotInline,)
+
+
+class ReservationServicesInline(admin.TabularInline):
+    model = ReservationContainsServices
+    extra = 1
 
 
 @admin.register(Reservation)
 class ReservationAdmin(admin.ModelAdmin):
-    list_display = ('id', 'stylist', 'customer', 'timestamp', )
+    list_display = ('id', 'stylist', 'customer', 'date', 'startTime', 'endTime', 'status')
+    inlines = (ReservationServicesInline,)
+    list_filter = ('status', 'date', )
 
 
 @admin.register(Service)
 class ServiceAdmin(admin.ModelAdmin):
     list_display = ('serviceName', 'defaultDuration', 'description', )
-
-admin.site.register(User)
-# admin.site.register()
